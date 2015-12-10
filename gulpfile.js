add827e894257cc242db23e578e806db3b432bae
tomltd <tomfreestone@gmail.com>
2015-12-10 13:23:47 +0000

    gulp with sass now working

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp');
 var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
 	return gulp.src('css/*.css')
 		.pipe(minifyCss({compatibility: 'ie8'}))
 		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', function() {
+	gulp.src('./sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+	gulp.watch('./sass/*.scss', ['sass']);
 });
